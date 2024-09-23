c150af058ab5f4fe974312bf693e7f4331c038eb
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-09-23 10:04:39 +0800

    chore: Update JWT configuration in production.rb for improved security and functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,15 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.jwt do
-    issuer 'aerostock-api'
-    secret Rails.application.credentials.jwt_secret
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 3.hours.to_i
   end
   # Code is not reloaded between requests.
   config.enable_reloading = false
